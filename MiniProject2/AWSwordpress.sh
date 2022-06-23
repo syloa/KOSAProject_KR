d18bb9f833b16421f562f1d0900657f78b63c171
DomingoS2 <jgim7357@gmail.com>
2022-06-24 01:22:39 +0900

    Update AWSwordpress.sh

--- MiniProject2/AWSwordpress.sh
+++ MiniProject2/AWSwordpress.sh
@@ -14,20 +14,22 @@ sentenforce 0
 
 sudo amazon-linux-extras enable php7.4
 
+sudo yum clean metadata
+
+sudo yum install -y php-cli php-pdo php-fpm php-json php-mysqlnd
+
 sudo yum install -y unzip wget httpd
 
 sudo systemctl enable --now httpd
 
-sudo yum clean metadata
-
-sudo yum install php-cli php-pdo php-fpm php-json php-mysqlnd
+cd /var/www/html
 
 sudo wget https://wordpress.org/wordpress-6.0.zip 
 
 sudo unzip wordpress-6.0.zip
 
-sudo mv ~/wordpress/* /var/www/html/
+sudo mv wordpress/* .
 
-sudo chown -R apache.apache /var/www/html/
+sudo chown -R apache.apache /var/www/*
 
 sudo systemctl restart httpd
